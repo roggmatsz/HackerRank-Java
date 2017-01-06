05ef0a6cfdeaf97b9081833391292463ed6f9018
Rogger Matamoros <roggmatz@gmail.com>
2017-01-05 22:15:06 -0500

    Initial given code commit.

--- io_one.java
+++ io_one.java
@@ -1,11 +1,13 @@
 //Reading input from stdin and stdout
+//goal: read 3 integers from stdin and 
+//      print 'em to stdout
 import java.util.*;
 
 public class io_one {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-
+        
         System.out.println(a);
     }
 }
