c3ce3f8f4186d373b8ba3a0ba5f1654d485a2cc4
Rogger Matamoros <roggmatz@gmail.com>
2017-01-05 22:10:44 -0500

    Initial given code commit.

---
+++ io_one.java
@@ -0,0 +1,11 @@
+//Reading input from stdin and stdout
+import java.util.*;
+
+public class io_one {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        System.out.println(a);
+    }
+}
