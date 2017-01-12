1167b8032c6fe96b3ea9276551d9cb390e84dab3
Rogger Matamoros <roggmatz@gmail.com>
2017-01-12 12:06:31 -0500

    Fixed Typo.

---
+++ if_else.java
@@ -0,0 +1,19 @@
+//Java If-else
+//Goal: 
+
+import java.io.*;
+import java.util.*;
+
+public class if_else {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        String answer = "";
+        if(n % 2 == 1) {
+            answer = "Weird";
+        } else {
+
+        }
+        System.out.println(answer);
+    }
+}
